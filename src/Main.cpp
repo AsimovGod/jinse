9dbdbfaf6ec53200f0e4827847b87c08c406440e
AsimovGod <AsimovGod@outlook.com>
2026-03-24 20:43:32 +0800

    add: clangd flags

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+#include <QApplication>
+#include <QWidget>
 
-using namespace std;
-
-int main()
+int main(int ViArgs, char* AcArgs[])
 {
+    QApplication EqApplication(ViArgs, AcArgs);
+    QWidget EqWindow;
+    EqWindow.setWindowTitle("Qt");
+    EqWindow.resize(400, 300);
+    EqWindow.show();
+    return EqApplication.exec();
 }
